d5fa1a9f140dfc443f5411bcf1d8527899b6addf
Harp3rDev <51060508+Harp3rDev@users.noreply.github.com>
2026-08-16 21:49:41 -0400

    Delete configs

--- archive/configs/sway/sway/scripts/template.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-SWAY=/home/amy/.config/sway/themes/template.conf
-WAYBAR=/home/amy/.config/waybar/themes/template.css
-WOFI=/home/amy/.config/wofi/themes/template.css
-MAKO=/home/amy/.config/mako/themes/template.conf
-
-cat $SWAY $WAYBAR $WOFI $MAKO | wl-copy
-
-echo 'Copied templates to clipboard'
